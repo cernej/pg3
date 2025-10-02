6f479ee899ba9539be7b03cf7cb50cb22ef346b5
cernej <cernej@email.cz>
2025-10-02 12:15:41 +0000

    prvni cviceni

---
+++ cviceni1.py
@@ -0,0 +1,18 @@
+# funkce porovna promennou hodnota s 3 a zjisti, zda je mensi, vetsi nebo rovna
+def cislo_mensi_nez_3(hodnota):
+    if hodnota > 3:
+        print(f"Hodnota {hodnota} je vetsi nez 3")
+    elif hodnota < 3:
+        print(f"Hodnota {hodnota} je mensi nez 3")
+    else:
+        print(f"Hodnota {hodnota} je rovna 3")
+
+
+if __name__ == "__main__":
+
+    cislo = input("Zadej cislo: ")
+    cislo = int(cislo)
+    print(f"Zadane cislo je: {cislo}")
+
+    cislo_mensi_nez_3(1)
+    cislo_mensi_nez_3(cislo)

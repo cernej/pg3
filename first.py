eb716078e65172104c78217e19fe8f0f82cccb13
cernej <cernej@email.cz>
2025-10-02 12:21:53 +0000

    prvni ukol

---
+++ first.py
@@ -0,0 +1,10 @@
+# funkce zkontroluje, zda je cislo suden nebo liche
+# a vypise:
+# - "Cislo X je sude"
+# - "Cislo X je liche"
+def sudy_nebo_lichy(cislo):
+    pass
+
+if __name__ == "__main__":
+    sudy_nebo_lichy(5)
+    sudy_nebo_lichy(1000000)
